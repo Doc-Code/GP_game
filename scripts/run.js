ee1d92daee32c5451a0021b7f6462cfa26b13a8e
drcode <stweb.it@gmail.com>
2022-12-14 23:01:14 +0300

    Setup data for character NFTs

--- scripts/run.js
+++ scripts/run.js
@@ -1,18 +1,27 @@
 const main = async () => {
-    const gameContractFactory = await hre.ethers.getContractFactory('MyGpGame');
-    const gameContract = await gameContractFactory.deploy();
-    await gameContract.deployed();
-    console.log("Contract deployed to:", gameContract.address);
-  };
-  
-  const runMain = async () => {
-    try {
-      await main();
-      process.exit(0);
-    } catch (error) {
-      console.log(error);
-      process.exit(1);
-    }
-  };
-  
-  runMain();
+  const gameContractFactory = await hre.ethers.getContractFactory("MyGpGame");
+  const gameContract = await gameContractFactory.deploy(
+    ["Lana", "Eva", "Riley"],
+    [
+      "https://get.wallhere.com/photo/Lana-Rhoades-digital-painting-digital-art-art-installation-Girl-High-juice-1886091.jpg",
+      "https://i.pinimg.com/originals/62/c3/46/62c34667207e44a79643b171374a93b8.jpg",
+      "https://i.ebayimg.com/00/s/MTYwMFgxMjgw/z/ICQAAOSwmQNfZCaa/$_57.PNG",
+    ],
+    [500, 1000, 1500],
+    [500, 250, 100]
+  );
+  await gameContract.deployed();
+  console.log("Contract deployed to:", gameContract.address);
+};
+
+const runMain = async () => {
+  try {
+    await main();
+    process.exit(0);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
+
+runMain();
